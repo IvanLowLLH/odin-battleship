b62d560aeb7d54281ba15ec83fcc35d99e2d1949
IvanLowLLH <ivanlowlh@gmail.com>
2025-11-25 13:50:46 +0800

    Add time delay when CPU turn

--- ui.js
+++ ui.js
@@ -2,6 +2,7 @@ import * as gameLogic from "./gameLogic.js"
 
 const userBoard = document.getElementById("user-board");
 const enemyBoard = document.getElementById("enemy-board");
+let cpuTurnNow = false;
 
 function renderGameboard() {
     // Render user player board;
@@ -33,29 +34,31 @@ function renderGameboard() {
     })
 }
 
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function clickBoard(event) {
     const selectedRow = event.target.dataset.rowIndex;
     const selectedCol = event.target.dataset.colIndex;
 
-    if (!selectedRow || !selectedCol) {
+    if (!selectedRow || !selectedCol || cpuTurnNow) {
         return
     }
     const hitResult = gameLogic.userPlayRound(selectedRow, selectedCol);
     if (hitResult === null) {  // invalid hit
         return
     }
-    updateBoard("enemy",hitResult, selectedRow, selectedCol);
+    updateBoard("enemy", hitResult, selectedRow, selectedCol);
     cpuTurn();
 }
 
 async function cpuTurn() {
-    // await sleep(3000);
+    cpuTurnNow = true;
+    await sleep(2000);
     const [cpuHitResult, cpuSelectedRow, cpuselectedCol] = gameLogic.cpuPlayRound();
     updateBoard("user", cpuHitResult, cpuSelectedRow, cpuselectedCol);
+    cpuTurnNow = false;
 }
 
 function updateBoard(boardName, hitResult, selectedRow, selectedCol) {
