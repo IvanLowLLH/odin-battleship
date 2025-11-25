fcbb0fa586039aee0d4d77ebbf18f14cff5746fc
IvanLowLLH <ivanlowlh@gmail.com>
2025-11-25 11:06:21 +0800

    Add UI function to render initial board

--- ui.js
+++ ui.js
@@ -1,15 +1,38 @@
 import * as gameLogic from "./gameLogic.js"
 
-const playerBoard = document.getElementById("user-player");
-const cpuBoard = document.getElementById("cpu-player");
+const userBoard = document.getElementById("user-board");
+const enemyBoard = document.getElementById("enemy-board");
 
 function renderGameboard() {
     // Render user player board;
-    playerBoard.textContent = "";
-    const playergameBoard = gameLogic.getUserPlayer();
+    userBoard.textContent = "";
+    const playergameBoard = gameLogic.getUserPlayer().gameboard.getBoard();
     
     playergameBoard.forEach((row, rowIndex) => {
-        row.forEach((cell, colIndex))
+        row.forEach((cell, colIndex) => {
+            const cellButton = document.createElement("button");
+            cellButton.classList.add("cell");
+            cellButton.dataset.rowIndex = rowIndex;
+            cellButton.dataset.colIndex = colIndex;
+            if (typeof cell === "object") {  // Cell contains ship
+                cellButton.classList.add("ship");
+            }
+            userBoard.appendChild(cellButton);
+        })
     })
 
+    // Render enemy board
+    playergameBoard.forEach((row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+            const cellButton = document.createElement("button");
+            cellButton.classList.add("cell");
+            cellButton.dataset.rowIndex = rowIndex;
+            cellButton.dataset.colIndex = colIndex;
+            enemyBoard.appendChild(cellButton);
+        })
+    })
+}
+
+export function loadUI() {
+    renderGameboard();
 }
